28edcc484f7dda54b184aaa65022784547872fc8
cainianhua <cainianhua@gmail.com>
2014-04-16 09:29:23 +0800

    Solved a wrong path.

--- YiJingWebUI/Cases/Detail.aspx.cs
+++ YiJingWebUI/Cases/Detail.aspx.cs
@@ -107,8 +107,8 @@ namespace YiJingWebUI.Cases
 		private void BindDataToPager() {
 			ltrCurrPageIndex.Text = this.CurrPageIndex.ToString();
 			ltrTotalCount.Text = this.TotalCount.ToString();
-			lnkPrevious.NavigateUrl = String.Format( "/news/detail.aspx?pn={0}", Math.Max( 1, this.CurrPageIndex - 1 ) );
-			lnkNext.NavigateUrl = String.Format( "/news/detail.aspx?pn={0}", Math.Min( this.CurrPageIndex + 1, this.TotalCount ) );
+			lnkPrevious.NavigateUrl = String.Format( "/cases/detail.aspx?pn={0}", Math.Max( 1, this.CurrPageIndex - 1 ) );
+			lnkNext.NavigateUrl = String.Format( "/cases/detail.aspx?pn={0}", Math.Min( this.CurrPageIndex + 1, this.TotalCount ) );
 		}
 	}
 }
