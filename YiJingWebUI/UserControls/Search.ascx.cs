15280bbbd1857fe2adaed56cd6a37509456e8d0e
Richard Cai <cainianhua@gmail.com>
2014-05-23 10:57:54 +0800

    Fixed a bug

--- YiJingWebUI/UserControls/Search.ascx.cs
+++ YiJingWebUI/UserControls/Search.ascx.cs
@@ -24,7 +24,9 @@ namespace YiJingWebUI.UserControls
 		protected void btnSearch_OnClick( object sender, EventArgs e ) {
 			string searchText = this.SearchText.Text.Trim();
 			if ( !string.IsNullOrEmpty( searchText ) )
-				Response.Redirect( String.Format( "/search/?s={0}", searchText ) );
+				Response.Redirect( String.Format( "/search/?s={0}", searchText ), true );
+			else
+				Response.Redirect( "/", true );
 		}
 	}
 }
