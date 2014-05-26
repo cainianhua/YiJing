0806896e89ca9b3d2f20b5cd360057bf69fc2379
Richard Cai <cainianhua@gmail.com>
2014-05-26 19:40:42 +0800

    fixed a bug

--- YiJingWebUI/Scripts/yijing/carousel_ajax.js
+++ YiJingWebUI/Scripts/yijing/carousel_ajax.js
@@ -8,273 +8,270 @@
  *           so it only can display article data, no swipe and drag effects.
  **/
 function Carousel(element, options) {
-	var _defaults = {
-		url: "/api/articledetail.ashx", // 获取数据的文章数据的API
-		containerSelector: ">div.panes",// Carousel的容器
-		paneItemsSelector: ">div.panes>div", // Carousel的数据项集合
-		paneItemTemplate: "<div class=\"pane pane-{index}\"><div class=\"maincontent\"><div id=\"loadingLayer\"></div><img id=\"loadingLayerImg\" alt=\"加载中...\" src=\"/content/images/ajax-loader.gif\" style=\"opacity:1; z-index:100001; position:absolute; {topLeftString}\" /></div></div>",
-		
-		totalPane: 0, 		// Carousel的总个数
-		currPaneIndex: 0, 	// 当前显示的Pane序号，从0开始计算
-		currSort: 0, 		// 当前页的根类编号
-		
-		onShowed: function () { },
-		onPageIndexChanged: function () { }
-	}
-	
-	element = $(element);
-	options = $.extend({}, _defaults, options);
-	
-	var self = this;
-	var paneStatuses = {};
-	var paneDatas = {};
+    var _defaults = {
+        url: "/api/articledetail.ashx", // 获取数据的文章数据的API
+        containerSelector: ">div.panes", // Carousel的容器
+        paneItemsSelector: ">div.panes>div", // Carousel的数据项集合
+        paneItemTemplate: "<div class=\"pane pane-{index}\"><div class=\"maincontent\"><div id=\"loadingLayer\"></div><img id=\"loadingLayerImg\" alt=\"加载中...\" src=\"/content/images/ajax-loader.gif\" style=\"opacity:1; z-index:100001; position:absolute; {topLeftString}\" /></div></div>",
 
-	var container = $(options.containerSelector, element);
-	var pane_width = 0;
-	var pane_count = options.totalPane;
-	var current_pane = options.currPaneIndex;
-	
-	/**
-	* initial
-	*/
-	this.init = function () {
-		var _topLeftString = topLeftString();
-		// generate empty Carousel element.
-		for (var i = 0; i < pane_count; i++) {
-			container.append(options.paneItemTemplate.replace(/\{index\}/g, i).replace(/\{topLeftString\}/g, _topLeftString));
-			paneStatuses["p" + i] = false;
-		}
+        totalPane: 0, 		// Carousel的总个数
+        currPaneIndex: 0, 	// 当前显示的Pane序号，从0开始计算
+        currSort: 0, 		// 当前页的根类编号
 
-		setPaneDimensions();
+        onShowed: function () { },
+        onPageIndexChanged: function () { }
+    }
 
-		$(window).on("load resize orientationchange", function () {
-			setPaneDimensions();
-		});
+    element = $(element);
+    options = $.extend({}, _defaults, options);
 
-		self.showPane(current_pane, true, true);
-	};
-	/**
-	* show pane by index
-	* parameters:
-	* @loadData: 是否需要加载数据，正常情况下都是需要加载的，如果在加载数据完成之后，
-	*            只是想显示当前pane，就不要重复触发数据加载，这个时候就要设置为false.
-	*/
-	this.showPane = function (index, animate, loadData) {
-		// between the bounds
-		index = Math.max(0, Math.min(index, pane_count - 1));
-		var indexChanged = index != current_pane;
-		current_pane = index;
+    var self = this;
+    var paneStatuses = {};
+    var paneDatas = {};
 
-		// load or preload data first.
-		loadData && loadArticles();
+    var container = $(options.containerSelector, element);
+    var pane_width = 0;
+    var pane_count = options.totalPane;
+    var current_pane = options.currPaneIndex;
 
-		// show current pane content.
-		var offset = -((100 / pane_count) * current_pane);
-		setContainerOffset(offset, animate);
+    /**
+    * initial
+    */
+    this.init = function () {
+        var _topLeftString = topLeftString();
+        // generate empty Carousel element.
+        for (var i = 0; i < pane_count; i++) {
+            container.append(options.paneItemTemplate.replace(/\{index\}/g, i).replace(/\{topLeftString\}/g, _topLeftString));
+            paneStatuses["p" + i] = false;
+        }
 
-		// do something when current index is changed.
-		if (indexChanged && paneStatuses["p" + current_pane] == true) {
-			// invoke page index changed callback.
-			if (options.onPageIndexChanged)
-				options.onPageIndexChanged(current_pane, paneDatas["_" + current_pane].title);
-		}
+        setPaneDimensions();
 
-		// set the pane height;
-		var pane_this = $(">div.pane:eq(" + current_pane + ")", container);
-		container.height(pane_this.attr("data-height"));
+        $(window).on("load resize orientationchange", function () {
+            setPaneDimensions();
+        });
 
-		if (options.onShowed)
-			options.onShowed(current_pane);
-	};
+        self.showPane(current_pane, true, true);
+    };
+    /*
+     * show pane by index
+     */
+    this.showPane = function (index, animate) {
+        // between the bounds
+        index = Math.max(0, Math.min(index, pane_count - 1));
+        var indexChanged = index != current_pane;
+        current_pane = index;
 
-	this.next = function () { return this.showPane(current_pane + 1, true, true); };
-	this.prev = function () { return this.showPane(current_pane - 1, true, true); };
+        // show current pane content.
+        var offset = -((100 / pane_count) * current_pane);
+        setContainerOffset(offset, animate);
 
-	/**
-	* set the pane dimensions and scale the container
-	*/
-	function setPaneDimensions() {
-		pane_width = element.width();
-		$(options.paneItemsSelector, element).each(function () {
-			$(this).width(pane_width);
-		});
-		container.width(pane_width * pane_count);
-	};
-	/**
-	* set the container offset to decide which pane to show.
-	*/
-	function setContainerOffset(percent, animate) {
-		container.removeClass("animate");
+        if (!paneStatuses["p" + current_pane]) {
+            LoadPaneDataAsync(current_pane, function () {
+                handlePaneShow(indexChanged);
+            });
+        }
+        else {
+            handlePaneShow(indexChanged);
+        }
+    };
 
-		if (animate) {
-			container.addClass("animate");
-		}
+    this.next = function () { return this.showPane(current_pane + 1, true, true); };
+    this.prev = function () { return this.showPane(current_pane - 1, true, true); };
 
-		if (Modernizr.csstransforms3d) {
-			container.css("transform", "translate3d(" + percent + "%,0,0) scale3d(1,1,1)");
-		}
-		else if (Modernizr.csstransforms) {
-			container.css("transform", "translate(" + percent + "%,0)");
-		}
-		else {
-			var px = ((pane_width * pane_count) / 100) * percent;
-			container.css("left", px + "px");
-		}
-	}
-	/**
-	* data provider, it will get data from a web api by ajax.
-	*/
-	function LoadPaneDataAsync(pane_index) {
-		if (pane_index < 0 && pane_index > pane_count - 1) return;
-		if (paneStatuses["p" + pane_index]) return;
+    /**
+    * set the pane dimensions and scale the container
+    */
+    function setPaneDimensions() {
+        pane_width = element.width();
+        $(options.paneItemsSelector, element).each(function () {
+            $(this).width(pane_width);
+        });
+        container.width(pane_width * pane_count);
+    };
+    /**
+    * set the container offset to decide which pane to show.
+    */
+    function setContainerOffset(percent, animate) {
+        container.removeClass("animate");
 
-		$.ajax({
-			url: options.url,
-			data: { pn: pane_index + 1, cid: options.currSort },
-			dataType: "json",
-			error: function (jqXHR, textStatus, errorThrown) { },
-			//complete: function (jqXHR, textStatus) { loading = false; },
-			success: function (data, status) {
-				if (data.code > 0) {
-					paneDatas["_" + pane_index] = data.dataItem;
-					_loadPaneDataHandler(pane_index, data.dataItem);
-				}
-			}
-		});
-	}
+        if (animate) {
+            container.addClass("animate");
+        }
 
-	function _loadPaneDataHandler(pane_index, item) {
-		if (item == null) return;
+        if (Modernizr.csstransforms3d) {
+            container.css("transform", "translate3d(" + percent + "%,0,0) scale3d(1,1,1)");
+        }
+        else if (Modernizr.csstransforms) {
+            container.css("transform", "translate(" + percent + "%,0)");
+        }
+        else {
+            var px = ((pane_width * pane_count) / 100) * percent;
+            container.css("left", px + "px");
+        }
+    }
+    /**
+    * Remarks: data provider, it will get data from a web api by ajax.
+    * @pane_index: the pane index needed to load.
+    * @callback: callback function after loaded data.
+    */
+    function LoadPaneDataAsync(pane_index, callback) {
+        if (pane_index < 0 && pane_index > pane_count - 1) return;
+        if (paneStatuses["p" + pane_index]) return;
 
-		var pane_this = $(">div.pane:eq(" + pane_index + ")", container);
+        $.ajax({
+            url: options.url,
+            data: { pn: pane_index + 1, cid: options.currSort },
+            dataType: "json",
+            error: function (jqXHR, textStatus, errorThrown) { },
+            //complete: function (jqXHR, textStatus) { loading = false; },
+            success: function (data, status) {
+                if (data.code > 0) {
+                    paneDatas["_" + pane_index] = data.dataItem;
+                    _loadPaneDataHandler(pane_index, data.dataItem);
+                    callback && callback();
+                }
+            }
+        });
+    }
 
-		var o = $(item.articleHtml);
-		var images = o.find("img");
-		var image_loaded_count = 0;
-		var image_count = images.length;
-		//console.log(image_count);
-		images.on("load", function (e) {
-			//console.log(image_loaded_count++);
-			image_loaded_count++
-			if (image_loaded_count == image_count) {
-				//console.log("done(" + pane_index + ")");
-				var pane_height = pane_this.height();
-				pane_this.attr("data-height", pane_height);
-				//console.log("height(" + pane_index + "):" + pane_height);
-				if (pane_index == current_pane) {
-					container.height(pane_height);
-				}
-				image_loaded_count = image_count = 0;
-			}
-		});
+    function _loadPaneDataHandler(pane_index, item) {
+        if (item == null) return;
 
-		pane_this.html(item.articleHtml);
+        var pane_this = $(">div.pane:eq(" + pane_index + ")", container);
 
-		pane_this.attr("data-height", pane_this.height());
+        var o = $(item.articleHtml);
+        var images = o.find("img");
+        var image_loaded_count = 0;
+        var image_count = images.length;
+        //console.log(image_count);
+        images.on("load", function (e) {
+            //console.log(image_loaded_count++);
+            image_loaded_count++
+            if (image_loaded_count == image_count) {
+                //console.log("done(" + pane_index + ")");
+                var pane_height = pane_this.height();
+                pane_this.attr("data-height", pane_height);
+                //console.log("height(" + pane_index + "):" + pane_height);
+                if (pane_index == current_pane) {
+                    container.height(pane_height);
+                }
+                image_loaded_count = image_count = 0;
+            }
+        });
 
-		//var bgStyle = "background:#" + item.bgcolor + " " + (item.bgpic ? "url(" + item.bgpic + ")" : "none") + " repeat-x center top;";
-		//pane_this.attr("style", bgStyle + pane_this.attr);
-		pane_this.css("background-color", "#" + item.bgcolor);
-		pane_this.css("background-image", (item.bgpic ? "url(" + item.bgpic + ")" : "none"));
-		pane_this.css("background-repeat", "repeat-x");
-		pane_this.css("background-position", "center top");
+        pane_this.html(item.articleHtml);
 
-		paneStatuses["p" + pane_index] = true;
+        pane_this.attr("data-height", pane_this.height());
 
-		// only current page need to show, and no need to load data again.
-		if (pane_index == current_pane) {
-			self.showPane(pane_index, true, false);
-			// invoke page index changed callback.
-			if (options.onPageIndexChanged)
-				options.onPageIndexChanged(current_pane, item.title);
-		}
-	}
+        //var bgStyle = "background:#" + item.bgcolor + " " + (item.bgpic ? "url(" + item.bgpic + ")" : "none") + " repeat-x center top;";
+        //pane_this.attr("style", bgStyle + pane_this.attr);
+        pane_this.css("background-color", "#" + item.bgcolor);
+        pane_this.css("background-image", (item.bgpic ? "url(" + item.bgpic + ")" : "none"));
+        pane_this.css("background-repeat", "repeat-x");
+        pane_this.css("background-position", "center top");
 
-	function _checkPaneDataLoadedStatus(pane_index) {
-		if (pane_index < 0 && pane_index > pane_count - 1) {
-			return true; // not existed pane, we set it to true, means that it no need to load.
-		}
-		else if (pane_index == 0) {
-			return paneStatuses["p" + current_pane] && paneStatuses["p" + (current_pane + 1)];
-		}
-		else if (pane_index == pane_count - 1) {
-			return paneStatuses["p" + current_pane] && paneStatuses["p" + (current_pane - 1)];
-		}
-		else {
-			return paneStatuses["p" + current_pane] && paneStatuses["p" + (current_pane - 1)] && paneStatuses["p" + (current_pane + 1)];
-		}
-	}
+        paneStatuses["p" + pane_index] = true;
+    }
 
-	function topLeftString() {
-		var top = 'top: ' + ((document.documentElement.clientHeight - 20) / 2 + document.documentElement.scrollTop) + 'px;';
-		var left = 'left: ' + (document.documentElement.clientWidth - 160) / 2 + 'px;';
-		return top + left;
-	}
+    function _checkPaneDataLoadedStatus(pane_index) {
+        if (pane_index < 0 && pane_index > pane_count - 1) {
+            return true; // not existed pane, we set it to true, means that it no need to load.
+        }
+        else if (pane_index == 0) {
+            return paneStatuses["p" + current_pane] && paneStatuses["p" + (current_pane + 1)];
+        }
+        else if (pane_index == pane_count - 1) {
+            return paneStatuses["p" + current_pane] && paneStatuses["p" + (current_pane - 1)];
+        }
+        else {
+            return paneStatuses["p" + current_pane] && paneStatuses["p" + (current_pane - 1)] && paneStatuses["p" + (current_pane + 1)];
+        }
+    }
 
-	function loadArticles() {
-		if (!paneStatuses["p" + current_pane]) {
-			LoadPaneDataAsync(current_pane);
-		}
-		// delay 3s to preload articles.
-		setTimeout(function () {
-			// Modernizr.hisotry detect if window.history.pushState is workable.
-			// if it is false, we will refresh page to load article,
-			// so pre-load is not needed.
-			if (Modernizr.history && current_pane - 1 >= 0 && !paneStatuses["p" + (current_pane - 1)]) {
-				LoadPaneDataAsync(current_pane - 1);
-			}
-			if (Modernizr.history && current_pane + 1 <= pane_count - 1 && !paneStatuses["p" + (current_pane + 1)]) {
-				LoadPaneDataAsync(current_pane + 1);
-			} 
-		}, 2500);
-	}
-	//
-	function handleHammer(ev) {
-		// disable browser scrolling
-		ev.gesture.preventDefault();
+    function topLeftString() {
+        var top = 'top: ' + ((document.documentElement.clientHeight - 20) / 2 + document.documentElement.scrollTop) + 'px;';
+        var left = 'left: ' + (document.documentElement.clientWidth - 160) / 2 + 'px;';
+        return top + left;
+    }
+    /*
+     *@indexChanged: to indicate if current page index is changed.
+     */
+    function handlePaneShow(indexChanged) {
+        // do something when current index is changed.
+        if (indexChanged) {
+            // invoke page index changed callback.
+            if (options.onPageIndexChanged)
+                options.onPageIndexChanged(current_pane, paneDatas["_" + current_pane].title);
+        }
 
-		switch (ev.type) {
-			case 'dragright':
-			case 'dragleft':
-				// stick to the finger
-				var pane_offset = -(100 / pane_count) * current_pane;
-				var drag_offset = ((100 / pane_width) * ev.gesture.deltaX) / pane_count;
+        // set the pane height;
+        var pane_this = $(">div.pane:eq(" + current_pane + ")", container);
+        container.height(pane_this.attr("data-height"));
 
-				// slow down at the first and last pane
-				if ((current_pane == 0 && ev.gesture.direction == "right") ||
+        if (options.onShowed)
+            options.onShowed(current_pane);
+        
+        // delay 0.5s to pre-load article data.
+        setTimeout(function () {
+            // Modernizr.hisotry detect if window.history.pushState is workable.
+            // if it is false, we will refresh page to load article,
+            // so pre-load is not needed.
+            if (Modernizr.history && current_pane - 1 >= 0 && !paneStatuses["p" + (current_pane - 1)]) {
+                LoadPaneDataAsync(current_pane - 1);
+            }
+            if (Modernizr.history && current_pane + 1 <= pane_count - 1 && !paneStatuses["p" + (current_pane + 1)]) {
+                LoadPaneDataAsync(current_pane + 1);
+            } 
+        }, 500);
+    }
+    //
+    function handleHammer(ev) {
+        // disable browser scrolling
+        ev.gesture.preventDefault();
+
+        switch (ev.type) {
+            case 'dragright':
+            case 'dragleft':
+                // stick to the finger
+                var pane_offset = -(100 / pane_count) * current_pane;
+                var drag_offset = ((100 / pane_width) * ev.gesture.deltaX) / pane_count;
+
+                // slow down at the first and last pane
+                if ((current_pane == 0 && ev.gesture.direction == "right") ||
                         (current_pane == pane_count - 1 && ev.gesture.direction == "left")) {
-					drag_offset *= .4;
-				}
+                    drag_offset *= .4;
+                }
 
-				setContainerOffset(drag_offset + pane_offset);
-				break;
+                setContainerOffset(drag_offset + pane_offset);
+                break;
 
-			case 'swipeleft':
-				self.next();
-				ev.gesture.stopDetect();
-				break;
+            case 'swipeleft':
+                self.next();
+                ev.gesture.stopDetect();
+                break;
 
-			case 'swiperight':
-				self.prev();
-				ev.gesture.stopDetect();
-				break;
+            case 'swiperight':
+                self.prev();
+                ev.gesture.stopDetect();
+                break;
 
-			case 'release':
-				// more then 50% moved, navigate
-				if (Math.abs(ev.gesture.deltaX) > pane_width / 2) {
-					if (ev.gesture.direction == 'right') {
-						self.prev();
-					} else {
-						self.next();
-					}
-				}
-				else {
-					self.showPane(current_pane, true, false);
-				}
-				break;
-		}
-	}
+            case 'release':
+                // more then 50% moved, navigate
+                if (Math.abs(ev.gesture.deltaX) > pane_width / 2) {
+                    if (ev.gesture.direction == 'right') {
+                        self.prev();
+                    } else {
+                        self.next();
+                    }
+                }
+                else {
+                    self.showPane(current_pane, true, false);
+                }
+                break;
+        }
+    }
 
-	//new Hammer(element[0], { drag_lock_to_axis: true }).on("release dragleft dragright swipeleft swiperight", handleHammer);
-	element.hammer({ drag_lock_to_axis: true }).on("release dragleft dragright swipeleft swiperight", handleHammer);
+    //new Hammer(element[0], { drag_lock_to_axis: true }).on("release dragleft dragright swipeleft swiperight", handleHammer);
+    element.hammer({ drag_lock_to_axis: true }).on("release dragleft dragright swipeleft swiperight", handleHammer);
 }
